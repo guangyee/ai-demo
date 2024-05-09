8f2ef5eb73573d470f7309105bec34dbb81cb08e
yeey <gyee@suse.com>
2024-05-09 15:52:33 -0700

    test

--- model/image_classification.py
+++ model/image_classification.py
@@ -62,14 +62,11 @@ normalization_layer = layers.Rescaling(1./255)
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 image_batch, labels_batch = next(iter(normalized_ds))
 first_image = image_batch[0]
-# Notice the pixel values are now in `[0,1]`.
-#print(np.min(first_image), np.max(first_image))
 
 num_classes = len(class_names)
 
 model = Sequential([
-  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-  #resize_and_rescale,
+  resize_and_rescale,
   #data_augmentation,
   layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
